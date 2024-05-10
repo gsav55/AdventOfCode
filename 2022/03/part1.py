0533d72e34f414109184115b40c322f6f4e248d1
gsav55 <graysav@gmail.com>
2024-05-09 23:16:59 -0500

    Day 3
    Continued work on day 3.

--- 2022/03/part1.py
+++ 2022/03/part1.py
@@ -5,6 +5,17 @@ EXAMPLE = "./example.txt"
 
 FILE = EXAMPLE
 
+alfa = []
+
+def get_alfa()-> list[str]:
+    global alfa
+    with open('alph.txt', 'r') as f:
+        for letter in f.readlines():
+            alfa.append(letter)
+    return alfa
+
+alfa = list(enumerate(alfa, 1))
+
 with open(FILE, "r") as f:
     for sack in f.readlines():
         print(sack)
@@ -15,4 +26,5 @@ with open(FILE, "r") as f:
         print(compartment2)
         for item in compartment1:
             if item in compartment2:
-
+                print(item)
+                pass
