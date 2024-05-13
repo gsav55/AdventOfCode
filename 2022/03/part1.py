15323457d61a37ecb3e88f7cd921432a0ed06ced
Grayson <graysav@gmail.com>
2024-05-13 17:40:01 -0500

    forgot to save last week

--- 2022/03/part1.py
+++ 2022/03/part1.py
@@ -1,30 +1,36 @@
-'''
+"""
 https://adventofcode.com/2022/day/3
-'''
-EXAMPLE = "./example.txt"
+"""
 
-FILE = EXAMPLE
+EXAMPLE = "2022\\03\\example.txt"
+INPUT = "2022\\03\\input.txt"
+ALPHABET = "2022\\03\\alph.txt"
+FILE = INPUT
 
-alfa = []
 
-def get_alfa()-> list[str]:
-    global alfa
-    with open('alph.txt', 'r') as f:
+def get_alfa() -> list[str]:
+    alfa = []
+    with open(ALPHABET, "r") as f:
         for letter in f.readlines():
-            alfa.append(letter)
+            alfa.append(letter.replace("\n", ""))
     return alfa
 
-alfa = list(enumerate(alfa, 1))
 
+alfa = get_alfa()
+value = 0
 with open(FILE, "r") as f:
     for sack in f.readlines():
         print(sack)
         num_items = len(sack)
-        compartment1 = sack[0:int(num_items/2)]
-        compartment2 = sack[int(num_items/2):num_items]
+        compartment1 = sack[0 : int(num_items / 2)]
+        compartment2 = sack[int(num_items / 2) : num_items]
         print(compartment1)
         print(compartment2)
         for item in compartment1:
             if item in compartment2:
                 print(item)
-                pass
+                print(alfa.index(item) + 1)
+                value += alfa.index(item) + 1
+                break
+
+print(f"Final Score: {value}")
